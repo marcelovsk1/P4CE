5ff08aeed7c1440201001ac834d7fb734d909e92
Marcelo Amaral Alves <marceloaa@live.com>
2024-06-17 23:56:58 -0400

    pace

---
+++ P4CE/P4CEApp.swift
@@ -0,0 +1,17 @@
+//
+//  P4CEApp.swift
+//  P4CE
+//
+//  Created by Marcelo Amaral Alves on 2024-06-17.
+//
+
+import SwiftUI
+
+@main
+struct P4CEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
